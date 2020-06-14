ee0ea650952ce54e63828cb6cd765bb028a48894
Alexey Kurin <alexey.kurin.nik@gmail.com>
2020-06-14 23:26:29 +0300

    Refactor tests

--- features/godogs.feature
+++ features/godogs.feature
@@ -7,3 +7,8 @@ Feature: eat godogs
     Given there are 12 godogs
     When I eat 5
     Then there should be 7 remaining
+
+  Scenario: Eat 1 out of 10
+    Given there are 10 godogs
+    When I eat 1
+    Then there should be 9 remaining
--- godogs_test.go
+++ godogs_test.go
@@ -4,40 +4,41 @@ import (
 	"fmt"
 
 	"github.com/cucumber/godog"
-	messages "github.com/cucumber/messages-go/v10"
+	"github.com/cucumber/messages-go/v10"
 )
 
-var Godogs int
+type state struct {
+	Godogs int
+}
 
-func thereAreGodogs(available int) error {
-	Godogs = available
+func (s *state) thereAreGodogs(available int) error {
+	s.Godogs = available
 	return nil
 }
 
-func iEat(num int) error {
-	if Godogs < num {
-		return fmt.Errorf("you cannot eat %d godogs, there are %d available", num, Godogs)
+func (s *state) iEat(num int) error {
+	if s.Godogs < num {
+		return fmt.Errorf("you cannot eat %d godogs, there are %d available", num, s.Godogs)
 	}
-	Godogs -= num
+	s.Godogs -= num
 	return nil
 }
 
-func thereShouldBeRemaining(remaining int) error {
-	if Godogs != remaining {
-		return fmt.Errorf("expected %d godogs to be remaining, but there is %d", remaining, Godogs)
+func (s *state) thereShouldBeRemaining(remaining int) error {
+	if s.Godogs != remaining {
+		return fmt.Errorf("expected %d godogs to be remaining, but there is %d", remaining, s.Godogs)
 	}
 	return nil
 }
 
-// godog v0.9.0 (latest) and earlier
 func FeatureContext(s *godog.Suite) {
-	s.BeforeSuite(func() { Godogs = 0 })
+	state := state{}
 
 	s.BeforeScenario(func(*messages.Pickle) {
-		Godogs = 0 // clean the state before every scenario
+		state.Godogs = 0
 	})
 
-	s.Step(`^there are (\d+) godogs$`, thereAreGodogs)
-	s.Step(`^I eat (\d+)$`, iEat)
-	s.Step(`^there should be (\d+) remaining$`, thereShouldBeRemaining)
+	s.Step(`^there are (\d+) godogs$`, state.thereAreGodogs)
+	s.Step(`^I eat (\d+)$`, state.iEat)
+	s.Step(`^there should be (\d+) remaining$`, state.thereShouldBeRemaining)
 }
